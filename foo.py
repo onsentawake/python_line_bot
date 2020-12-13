0e47a492fbe6860b6f00d1f9107a13a5ee6e5c5b
onsentawake <58753727+onsentawake@users.noreply.github.com>
2020-12-13 23:15:42 +0900

    first_push

---
+++ foo.py
@@ -0,0 +1,13 @@
+class Student:
+    def avg(self,unko,unnko):
+        print((unko + unnko) / 2)
+
+a001 = Student()
+a001.avg(324,3245)
+
+a001.unnnko = "baaaan"
+print(a001.unnnko)
+
+a002 = Student()
+a002.bitiguso = "mitimiti"
+print(a002.bitiguso)
